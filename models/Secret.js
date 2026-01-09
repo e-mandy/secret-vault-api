a4a143c571eb39ebeba0a00d785eb9f5eede43ad
e-mandy <andymfrd02@gmail.com>
2026-01-09 01:53:26 +0100

    Suppression de l'indexation du node module

---
+++ models/Secret.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const secretSchema = mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    }
+});
+
+export default mongoose.model('Secret', secretSchema);
+
