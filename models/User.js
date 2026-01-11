b86e044396ec51a2c161d39206769fea297a414f
e-mandy <andymfrd02@gmail.com>
2026-01-11 14:57:39 +0100

    Mise en place du model User

---
+++ models/User.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
+
+const userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        lowercase: true,
+        trim: true,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+    },
+    TwoFASecret: {
+        type: String,
+        default: null,
+    },
+    recoveryCodes: [
+        {
+            type: String,
+            unique: true
+        }
+    ]
+});
+
+userSchema.pre('save', async function(next){
+    if(!this.isModified('password')){
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(process.env.SALT);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.comparePassword = async function(password){
+    return await bcrypt.compare(password, this.password);
+};
+
+export default mongoose.model('User', userSchema);
