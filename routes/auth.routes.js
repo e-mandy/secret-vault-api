a09eccf07c076b69ab009e09174470d1510fad35
e-mandy <andymfrd02@gmail.com>
2026-01-11 15:30:25 +0100

    Mise en place du login

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import User from '../models/User';
-import jwt from 'jsonwebtoken';
+import jwt, { sign } from 'jsonwebtoken';
 
 const authRoutes = express.Router();
 
@@ -27,12 +27,12 @@ authRoutes.post('/register', async (req, res) => {
             httpOnly: true,
         });
 
-        res.status(200).json({
-            code: 200,
+        res.status(201).json({
+            code: 201,
             message: "User created successfully",
             data: {
                 userEmail: newUser.email,
-                token
+                accessToken
             }
         });
     }catch(error){
@@ -41,4 +41,39 @@ authRoutes.post('/register', async (req, res) => {
             message: error.message
         });
     }
+});
+
+
+authRoutes.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    // Ici on doit prend tout sauf le mot de passe (-password)
+    const user = await User.findOne({ email });
+
+    if(!user) return res.status(400).json({
+        code: 400,
+        message: "Invalid credentials"
+    });
+
+    if(!user.comparePassword(password)) return res.status(401).json({
+        code: 401,
+        message: "Invalid credentials"
+    });
+
+    const acces_token = jwt.sign({ userId: user._id }, process.env.SECRET_APP_KEY, { expiresIn: '15m' });
+
+    const refresh_token = jwt.sign({ userId: user._id }, process.env.SECRET_REFRESH_APP, { expiresIn: '7d' });
+
+    res.cookie('refreshToken', refresh_token, {
+        httpOnly: true
+    });
+
+    return res.status(200).json({
+        code: 200,
+        message: "User logged successfully",
+        data: {
+            userEmail: email,
+            acces_token
+        }
+    });
 });
